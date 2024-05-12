78b7faab33e43930f40ff564bfb5469e5d6f6490
ntEOS <nguyentoan231196@gmail.com>
2024-05-12 21:33:48 +0700

    describe if any err on declare and bind and implement subscription to client main

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqb "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	con, err := amqb.Dial(connectionStr)
+	if err != nil {
+		log.Fatalf("Cannot create channel connection: %v\n", err)
+	}
+
+	defer con.Close()
+	fmt.Println("Peril game connected to rabbitmq")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Could not get username: %v\n", err)
+	}
+
+	_, que, err := pubsub.DeclareAndBind(
+		con,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.SimpleQueueTrasient,
+	)
+	if err != nil {
+		log.Fatalf("Could not subscribe to pause: %v\n", err)
+	}
+
+	fmt.Printf("Queue %v declared and bound\n", que.Name)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
--- internal/pubsub/declare_bind.go
+++ internal/pubsub/declare_bind.go
@@ -1,25 +1,44 @@
 package pubsub
 
 import (
+	"log"
+
 	amqb "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpeQueueType int
+
+const (
+	SimpleQueueDurable SimpeQueueType = iota
+	SimpleQueueTrasient
 )
 
 func DeclareAndBind(
 	conn *amqb.Connection,
 	exchange, queueName, key string,
-	simpleQueueType int,
+	simpleQueueType SimpeQueueType,
 ) (*amqb.Channel, amqb.Queue, error) {
 	channel, err := conn.Channel()
 	if err != nil {
+		log.Printf("Cannot create channel: %v\n", err)
 		return nil, amqb.Queue{}, err
 	}
 
-	queue, err := channel.QueueDeclare(queueName, false, true, true, false, nil)
+	queue, err := channel.QueueDeclare(
+		queueName,
+		simpleQueueType == SimpleQueueDurable,
+		simpleQueueType != SimpleQueueDurable,
+		simpleQueueType != SimpleQueueDurable,
+		false,
+		nil,
+	)
 	if err != nil {
+		log.Printf("Cannot create queue: %v\n", err)
 		return nil, amqb.Queue{}, err
 	}
 
 	if err = channel.QueueBind(queueName, key, exchange, false, nil); err != nil {
+		log.Printf("Cannot bind queue and channel: %v\n", err)
 		return nil, amqb.Queue{}, nil
 	}
 
